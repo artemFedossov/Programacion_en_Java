e585f569f0a8def7e6d31df012f3b5d8b87c9248
Artem <a.fedossov@hotmail.com.ar>
2017-07-25 07:30:10 -0300

    Ejercicios de la clase 1

---
+++ introduccion_java/laboratorio/ejercicio_01/src/ejercicio_01/Ejercicio_01.java
@@ -0,0 +1,21 @@
+package ejercicio_01;
+
+public class Ejercicio_01 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Ejercico 1:\nDados\nnumero1 = 5\nnumero2 = 10\nnumero3 = 20\nInformar:\na) numero1 + numero2\tb) numero3 - numero1\tc) numero1 * numero3\td) numero3 / numero2");
+        int numero1 = 5;
+        int numero2 = 10;
+        int numero3 = 20;
+        
+        System.out.println("\na) = "+(numero1 + numero2));
+        System.out.println("b) = "+(numero3 - numero1));
+        System.out.println("c) = "+(numero1 * numero3));
+        System.out.println("d) = "+(numero3 / numero2));
+        
+       
+       
+    }
+    
+}
